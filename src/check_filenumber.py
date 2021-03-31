4a16c42302e0d4c0af1a60a809a800ea78122e1f
FOehlschlaeger <friso.oehlschlaeger@gmail.com>
2021-03-31 11:34:22 +0200

    adding function and unittest for string space cleaning in txt file in check_filenumber.py und test_check_filenumber.py

--- src/check_filenumber.py
+++ src/check_filenumber.py
@@ -4,6 +4,28 @@ import random
 
 random.seed(1)
 
+def del_spaces(filepath: str) -> str:
+    """
+    del_spaces deletes spaces in given txt file between all characters
+
+    Args:
+        filepath (str): path to the file to be cleaned
+
+    Returns:
+        str: string to the cleaned file while keeping the original file untouched
+    """
+    filepath_cleaned = os.path.splitext(filepath)[0] + "_cleaned" + os.path.splitext(filepath)[1]
+    with open(filepath, "r", encoding="utf-16le") as orig_file:
+        with open(filepath_cleaned, "w", encoding="utf-16le") as file_fixed:
+            newline = ""
+            for line in orig_file:
+                entry_list = line.split("\t")
+                fixed_entry_list = [x[::2] for x in entry_list]
+                fixed_line = "\t".join(fixed_entry_list)
+                file_fixed.write(newline + fixed_line)
+                newline = "\n"
+    return filepath_cleaned
+
 
 def make_path(filepath: str, folder: str) -> str:
     if filepath.endswith("/"):
