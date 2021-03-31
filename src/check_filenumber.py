eef24061f9140ce1b803d48b084313800e8a0e50
FOehlschlaeger <friso.oehlschlaeger@gmail.com>
2021-03-31 11:53:13 +0200

    cleaning scripts from whitespaces etc.

--- src/check_filenumber.py
+++ src/check_filenumber.py
@@ -14,12 +14,12 @@ def make_path(filepath: str, folder: str) -> str:
     return f"{filepath}/{folder}"
 
 
-def create_files(filepath: str, number: int, limit: int) -> None: 
+def create_files(filepath: str, number: int, limit: int) -> None:
     path_to_folder = make_path(filepath, "test")
     print(path_to_folder)
 
     for i in range(number):
-        with open(f"{path_to_folder}/testfile{i}.txt", "w") as file: 
+        with open(f"{path_to_folder}/testfile{i}.txt", "w") as file:
             for j in range(random.randrange(limit)):
                 file.write(f"tesfile{i}_{j}\n")
     return None
@@ -28,7 +28,7 @@ def create_files(filepath: str, number: int, limit: int) -> None:
 def more_lines_than_limit(file: str, limit: int) -> bool:
     if not isinstance(limit, int) or limit <= 0:
         raise ValueError("limit has to be an integer larger than zero. ")
-    
+
     numlines = sum(1 for line in open(file, "r"))
     if numlines > limit:
         default = True
@@ -37,7 +37,7 @@ def more_lines_than_limit(file: str, limit: int) -> bool:
     return default
 
 
-def main(): 
+def main():
     create_files("data", 3, 10)
 
     filelist = []
@@ -49,13 +49,13 @@ def main():
             filelist.append(full_path)
     print(f"files to check : {filelist}")
 
-    for files in filelist: 
+    for files in filelist:
         print(more_lines_than_limit(files, 5))
-    
+
     print(more_lines_than_limit('data/test\\testfile2.txt', "a"))
 
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
     print("starting... ")
     main()
     print("finished")
--- tests/test_del_space.py
+++ tests/test_del_space.py
@@ -26,7 +26,7 @@ def test_del_spaces():
     expected_hash = md5(dummy_file)
 
     dummy_file_spaces = os.path.join(path_to_testing, "dummy_file_spaces.txt")
-    with open(dummy_file_spaces, "w", encoding="utf-16le") as fs: 
+    with open(dummy_file_spaces, "w", encoding="utf-16le") as fs:
         fs.write("H a l l o" + "\t" + "W e l t" + "\t" + "1 2 3\n")
         fs.write("t e s t" + "\t" + "" + "\t" + "a b c d e f")
 
@@ -35,7 +35,7 @@ def test_del_spaces():
 
     with pytest.raises(TypeError):
         del_spaces(123)
-    with pytest.raises(FileNotFoundError): 
+    with pytest.raises(FileNotFoundError):
         del_spaces("test/to/missing/file.txt")
 
     # remove created dummy files
